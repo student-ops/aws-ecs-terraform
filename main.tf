ce1d9314be7e1811aba38cb22181f3fc84ca2035
student-ops <gogakuec@gmail.com>
2023-02-20 00:02:25 +0900

    a

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   # Assumes s3 bucket and dynamo DB table already set up
   backend "s3" {
-    bucket         = "aws-ecs-terraform-tfstate"
+    bucket         = "aws-ecs-terraform-tfstate-my-train"
     key            = "terraform.tfstate"
     region         = "ap-northeast-1"
     dynamodb_table = "aws-ecs-terraform-tfstate-locking"
