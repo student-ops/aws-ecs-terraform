1c3ccae57ca8e2e939f722552cd11f41560e6fb0
student-ops <gogakuec@gmail.com>
2023-02-20 02:21:09 +0900

    format

--- main.tf
+++ main.tf
@@ -7,14 +7,14 @@ terraform {
     dynamodb_table = "aws-ecs-terraform-tfstate-locking"
     encrypt        = true
   }
-   required_providers {
-   aws = {
-     source  = "hashicorp/aws"
-     version = "~> 4.0"
-   }
- }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 
 }
- provider "aws" {
-   region ="ap-northeast-1"
+provider "aws" {
+  region = "ap-northeast-1"
 }
